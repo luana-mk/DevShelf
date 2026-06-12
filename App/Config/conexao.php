7996fcb00040eabd6aebe370dde90d1677cdd526
OTHON AUGUSTO FREITAS NASCIMENTO <othon.nascimento@cs.up.edu.br>
2026-06-12 20:31:49 -0300

    fix: correção

--- App/Config/conexao.php
+++ App/Config/conexao.php
@@ -1,9 +1,9 @@
 <?php
-$host = 'localhost';
+$host = 'localhost:3307';
 $dbname = 'devshelf';
 $user = 'root';
 $pass = '';
 
-$pdo = new PDO("mysql:host=localhost;dbname=devshelf", "root", "123456");
+$pdo = new PDO("mysql:host=localhost;dbname=devshelf", "root", "");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
