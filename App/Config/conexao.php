b7744cfe7fb55fcbd72df3027927c8993dba2cc2
OTHON AUGUSTO FREITAS NASCIMENTO <othon.nascimento@cs.up.edu.br>
2026-06-12 20:06:27 -0300

    fix: correção root MySql

--- App/Config/conexao.php
+++ App/Config/conexao.php
@@ -4,6 +4,6 @@ $dbname = 'devshelf';
 $user = 'root';
 $pass = '';
 
-$pdo = new PDO("mysql:host=localhost;dbname=devshelf", "root", "root");
+$pdo = new PDO("mysql:host=localhost;dbname=devshelf", "root", "123456");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
